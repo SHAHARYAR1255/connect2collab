d398dcc3a9fe8298c7fe509d976025f323e562ae
HUNZALA MUSHTAQ <75185145+HUNZALAMUSHTAQ@users.noreply.github.com>
2021-07-03 22:33:28 +0500

    hunzala

--- src/views/DashboardStudent/DashboardStudent.js
+++ src/views/DashboardStudent/DashboardStudent.js
@@ -2,239 +2,216 @@ import React from "react";
 import ChartistGraph from "react-chartist";
 // react-bootstrap components
 import {
-    Badge,
-    Button,
-    Card,
-    Navbar,
-    Nav,
-    Table,
-    Container,
-    Row,
-    Col,
-    Form,
-    OverlayTrigger,
-    Tooltip,
+  Badge,
+  Button,
+  Card,
+  Navbar,
+  Nav,
+  Table,
+  Container,
+  Row,
+  Col,
+  Form,
+  OverlayTrigger,
+  Tooltip,
 } from "react-bootstrap";
 
+
 function Dashboard() {
-    return (
-        <>
-            <Container fluid>
-                <Row>
-                    <Col lg="3" sm="6">
-                        <Card className="card-stats">
-                            <Card.Body>
-                                <Row>
-                                    <Col xs="5">
-                                        <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-chart text-warning"></i>
-                                        </div>
-                                    </Col>
-                                    <Col xs="7">
-                                        <div className="numbers">
-                                            <p className="card-category">Number</p>
-                                            <Card.Title as="h4">150GB</Card.Title>
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                            <Card.Footer>
-                                <hr></hr>
-                                <div className="stats">
-                                    <i className="fas fa-redo mr-1"></i>
-                  Update Now
+  return (
+    <>
+      <div class="sidebar" data-image="../assets/img/sidebar-5.jpg">
+        <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+
+        Tip 2: you can also add an image using data-image tag
+     -->
+        <div class="sidebar-wrapper">
+          <div class="logo">
+            <a href="http://www.creative-tim.com" class="simple-text">
+              Connect2Collab
+            </a>
+          </div>
+          <ul class="nav">
+            <li class="nav-item ">
+              <a class="nav-link" href="dashboard.html">
+                <i class="nc-icon nc-chart-pie-35"></i>
+                <p>Dashboard</p>
+              </a>
+            </li>
+            <li>
+              <a class="nav-link" href="./user.html">
+                <i class="nc-icon nc-circle-09"></i>
+                <p>User Profile</p>
+              </a>
+            </li>
+            <li>
+              <a class="nav-link" href="./table.html">
+                <i class="nc-icon nc-notes"></i>
+                <p>Internships</p>
+              </a>
+            </li>
+            <li>
+              <a class="nav-link" href="./typography.html">
+                <i class="nc-icon nc-paper-2"></i>
+                <p>Mentors</p>
+              </a>
+            </li>
+            <li>
+              <a class="nav-link" href="./icons.html">
+                <i class="nc-icon nc-atom"></i>
+                <p>Companies</p>
+              </a>
+            </li>
+            <li>
+              <a class="nav-link" href="./maps.html">
+                <i class="nc-icon nc-pin-3"></i>
+                <p>Students</p>
+              </a>
+            </li>
+            <li>
+              <a class="nav-link" href="./notifications.html">
+                <i class="nc-icon nc-bell-55"></i>
+                <p>Notifications</p>
+              </a>
+            </li>
+            <li>
+              <a class="nav-link" href="./notifications.html">
+                <i class="nc-icon nc-bell-55"></i>
+                <p>Messages</p>
+              </a>
+            </li>
+            <li class="nav-item active active-pro">
+              <a class="nav-link active" href="upgrade.html">
+                <i class="nc-icon nc-alien-33"></i>
+                <p>Communities</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="main-panel">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg " color-on-scroll="500">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#pablo"> Dashboard </a>
+            <button href="" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+              aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-bar burger-lines"></span>
+              <span class="navbar-toggler-bar burger-lines"></span>
+              <span class="navbar-toggler-bar burger-lines"></span>
+            </button>
+
+          </div>
+        </nav>
+        <!-- End Navbar -->
+        <div class="content">
+          <!--WORKING AREA-->
+          <div class="container my-new-sec " id="ScrollBar-Design">
+
+
+            <ul class="nav justify-content-around sticky-top sub-nav ">
+
+
+              <li class="nav-item">
+                <a class="nav-link active active-link" href="#">Hired</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Review Application </a>
+              </li>
+            </ul>
+
+
+            <div class="container">
+
+              <!-- cart 1 -->
+              <div class="cart d-flex  flex-wrap justify-content-between">
+
+                <div class="d-flex cart-p1">
+                  <div class="cart-pic">
+                    <img src="ProfilePhoto (1).jpg" alt="profile-pic" />
+                  </div>
+                  <div class="cart-heading-details">
+                    <h2 class="cart-heading">Hunzala Mushtaq</h2>
+                    <p class="cart-details">Lot voluptates dolore cumque nesciunt est.</p>
+                  </div>
                 </div>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
-                    <Col lg="3" sm="6">
-                        <Card className="card-stats">
-                            <Card.Body>
-                                <Row>
-                                    <Col xs="5">
-                                        <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-light-3 text-success"></i>
-                                        </div>
-                                    </Col>
-                                    <Col xs="7">
-                                        <div className="numbers">
-                                            <p className="card-category">Revenue</p>
-                                            <Card.Title as="h4">$ 1,345</Card.Title>
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                            <Card.Footer>
-                                <hr></hr>
-                                <div className="stats">
-                                    <i className="far fa-calendar-alt mr-1"></i>
-                  Last day
+
+                <div class="cart-p2 d-flex flex-column">
+                  <h2 class="status">status</h2>
+                  <p class="status-details">Available </p>
                 </div>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
-                    <Col lg="3" sm="6">
-                        <Card className="card-stats">
-                            <Card.Body>
-                                <Row>
-                                    <Col xs="5">
-                                        <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-vector text-danger"></i>
-                                        </div>
-                                    </Col>
-                                    <Col xs="7">
-                                        <div className="numbers">
-                                            <p className="card-category">Errors</p>
-                                            <Card.Title as="h4">23</Card.Title>
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                            <Card.Footer>
-                                <hr></hr>
-                                <div className="stats">
-                                    <i className="far fa-clock-o mr-1"></i>
-                  In the last hour
+              </div>
+              <!-- cart end -->
+
+              <!-- cart 1 -->
+              <div class="cart d-flex  flex-wrap justify-content-between">
+
+                <div class="d-flex cart-p1">
+                  <div class="cart-pic">
+                    <img src="ProfilePhoto (1).jpg" alt="profile-pic" />
+                  </div>
+                  <div class="cart-heading-details">
+                    <h2 class="cart-heading">Hunzala Mushtaq</h2>
+                    <p class="cart-details">Lot voluptates dolore cumque nesciunt est.</p>
+                  </div>
                 </div>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
-                    <Col lg="3" sm="6">
-                        <Card className="card-stats">
-                            <Card.Body>
-                                <Row>
-                                    <Col xs="5">
-                                        <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-favourite-28 text-primary"></i>
-                                        </div>
-                                    </Col>
-                                    <Col xs="7">
-                                        <div className="numbers">
-                                            <p className="card-category">Followers</p>
-                                            <Card.Title as="h4">+45K</Card.Title>
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                            <Card.Footer>
-                                <hr></hr>
-                                <div className="stats">
-                                    <i className="fas fa-redo mr-1"></i>
-                                Update now
+
+                <div class="cart-p2 d-flex flex-column">
+                  <h2 class="status">status</h2>
+                  <p class="status-details">Available </p>
                 </div>
-                            </Card.Footer>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-          <Col md="8">
-            <Card>
-              <Card.Header>
-                <Card.Title as="h4">Users Behavior</Card.Title>
-                <p className="card-category">24 Hours performance</p>
-              </Card.Header>
-              <Card.Body>
-                <div className="ct-chart" id="chartHours">
-                  <ChartistGraph
-                    data={{
-                      labels: [
-                        "9:00AM",
-                        "12:00AM",
-                        "3:00PM",
-                        "6:00PM",
-                        "9:00PM",
-                        "12:00PM",
-                        "3:00AM",
-                        "6:00AM",
-                      ],
-                      series: [
-                        [287, 385, 490, 492, 554, 586, 698, 695],
-                        [67, 152, 143, 240, 287, 335, 435, 437],
-                        [23, 113, 67, 108, 190, 239, 307, 308],
-                      ],
-                    }}
-                    type="Line"
-                    options={{
-                      low: 0,
-                      high: 800,
-                      showArea: false,
-                      height: "245px",
-                      axisX: {
-                        showGrid: false,
-                      },
-                      lineSmooth: true,
-                      showLine: true,
-                      showPoint: true,
-                      fullWidth: true,
-                      chartPadding: {
-                        right: 50,
-                      },
-                    }}
-                    responsiveOptions={[
-                      [
-                        "screen and (max-width: 640px)",
-                        {
-                          axisX: {
-                            labelInterpolationFnc: function (value) {
-                              return value[0];
-                            },
-                          },
-                        },
-                      ],
-                    ]}
-                  />
+              </div>
+              <!-- cart end -->
+              <!-- cart 1 -->
+              <div class="cart d-flex  flex-wrap justify-content-between">
+
+                <div class="d-flex cart-p1">
+                  <div class="cart-pic">
+                    <img src="ProfilePhoto (1).jpg" alt="profile-pic" />
+                  </div>
+                  <div class="cart-heading-details">
+                    <h2 class="cart-heading">Hunzala Mushtaq</h2>
+                    <p class="cart-details">Lot voluptates dolore cumque nesciunt est.</p>
+                  </div>
                 </div>
-              </Card.Body>
-              <Card.Footer>
-                <div className="legend">
-                  <i className="fas fa-circle text-info"></i>
-                  Open <i className="fas fa-circle text-danger"></i>
-                  Click <i className="fas fa-circle text-warning"></i>
-                  Click Second Time
+
+                <div class="cart-p2 d-flex flex-column">
+                  <h2 class="status">status</h2>
+                  <p class="status-details">Available </p>
                 </div>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-history"></i>
-                  Updated 3 minutes ago
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col md="4">
-            <Card>
-              <Card.Header>
-                <Card.Title as="h4">Email Statistics</Card.Title>
-                <p className="card-category">Last Campaign Performance</p>
-              </Card.Header>
-              <Card.Body>
-                <div
-                  className="ct-chart ct-perfect-fourth"
-                  id="chartPreferences"
-                >
-                  <ChartistGraph
-                    data={{
-                      labels: ["40%", "20%", "40%"],
-                      series: [40, 20, 40],
-                    }}
-                    type="Pie"
-                  />
-                </div>
-                <div className="legend">
-                  <i className="fas fa-circle text-info"></i>
-                  Open <i className="fas fa-circle text-danger"></i>
-                  Bounce <i className="fas fa-circle text-warning"></i>
-                  Unsubscribe
-                </div>
-                <hr></hr>
-                <div className="stats">
-                  <i className="far fa-clock"></i>
-                  Campaign sent 2 days ago
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-            </Container>
+              </div>
+              <!-- cart end -->
+
+
+
+
+            </div>
+
+          </div>
+          <div class="container Pagination-flex">
+            <div class="pagination">
+              <a href="#">&laquo;</a>
+              <a href="#" class="active">1</a>
+              <a href="#" >2</a>
+              <a href="#">3</a>
+              <a href="#">4</a>
+              <a href="#">&raquo;</a>
+            </div></div>
+
+
+
+
+
+        </div>
+
+        <!--WORING END-->
+      </div>
+
+    </div>
+   </div >
+ 
+   </div >
         </>
     );
 }
